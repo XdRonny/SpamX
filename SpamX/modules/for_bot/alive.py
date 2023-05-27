d37eb1dc90c6bd4e425b93497cc5d039f6c04753
XdRonny <127653254+XdRonny@users.noreply.github.com>
2023-05-28 03:35:41 +0530

    Update alive.py

--- SpamX/modules/for_bot/alive.py
+++ SpamX/modules/for_bot/alive.py
@@ -106,8 +106,8 @@ async def SpamX_(SpamX: Client, message: Message):
        spamx_reply += f"**Total Sudos:** `{users_db.sudo_count()}` \n"
        spamx_reply += f"**Total Gbanned users:** `{gban_db.gban_count()}` \n"
     spamx_reply += "<=======================> \n\n"
-    spamx_reply += "**Source Code:** [GitHub 🐈‍⬛](https://github.com/RiZoeLX/SpamX) \n\n"
-    spamx_reply += "**© @RiZoeLX** || Support: @DNHxHELL"
+    spamx_reply += "**ᴏᴡɴᴇʀ:** [GitHub 🐈‍⬛](https://t.me/II_Ronny_II) \n\n"
+    spamx_reply += "**© @Bots_Hub_ll** || Support: @Daisy_Support_chat"
     
     try:
        await info.edit_text(spamx_reply, disable_web_page_preview=True)
